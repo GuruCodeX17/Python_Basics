860f67d46c482ac8426dad25a64df57b6c460879
Guru <guruarunachalam17@gmail.com>
2026-07-11 11:33:15 +0530

    Added variable.py

---
+++ variable.py
@@ -0,0 +1,20 @@
+#1. string
+name="Guru"
+
+#2. integer 
+age=19
+
+#3. float
+height=5.6
+
+#4. Boolean
+is_student=True
+
+#5. complex
+number=2+3j
+
+print("Name:",name)
+print("Age:",age)
+print("Height:",height)
+print("Student:",is_student)
+print("Complex:",number)
