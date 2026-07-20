31725c19816ab5caea3cfcab89389d50711351a0
Guru <guruarunachalam17@gmail.com>
2026-07-20 07:40:26 +0530

    Added input and output programs

---
+++ input_output.py
@@ -0,0 +1,47 @@
+# ----------------------------
+# Program 1: User Information
+# ----------------------------
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("\n--- User Information ---")
+print("Name:", name)
+print("Age:", age)
+
+
+# ----------------------------
+# Program 2: Add Two Numbers
+# ----------------------------
+
+num1 = int(input("\nEnter first number: "))
+num2 = int(input("Enter second number: "))
+
+sum = num1 + num2
+
+print("Sum =", sum)
+
+
+# ----------------------------
+# Program 3: Calculate Average
+# ----------------------------
+
+mark1 = float(input("\nEnter mark 1: "))
+mark2 = float(input("Enter mark 2: "))
+mark3 = float(input("Enter mark 3: "))
+
+average = (mark1 + mark2 + mark3) / 3
+
+print("Average =", average)
+
+
+# ----------------------------
+# Program 4: Rectangle Area
+# ----------------------------
+
+length = float(input("\nEnter length: "))
+width = float(input("Enter width: "))
+
+area = length * width
+
+print("Area of Rectangle =", area)
